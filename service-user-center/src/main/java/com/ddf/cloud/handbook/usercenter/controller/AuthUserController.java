182b73ea12c2731a34da7ba1e0944e50f7b76ffc
dongfang.ding <1041765757@qq.com>
2020-07-11 18:49:16 +0800

    待解决nacos config多namespace配置时，如果nacos的配置信息不放在bootstrap.yml中就不会生效

--- service-user-center/src/main/java/com/ddf/cloud/handbook/usercenter/controller/AuthUserController.java
+++ service-user-center/src/main/java/com/ddf/cloud/handbook/usercenter/controller/AuthUserController.java
@@ -1,14 +1,18 @@
 package com.ddf.cloud.handbook.usercenter.controller;
 
 import com.ddf.cloud.handbook.api.model.usercenter.AuthUser;
+import com.ddf.cloud.handbook.core.config.GlobalProperties;
 import com.ddf.cloud.handbook.core.response.ResponseData;
 import com.ddf.cloud.handbook.usercenter.service.AuthUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 /**
@@ -21,12 +25,23 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor(onConstructor=@__(@Autowired))
 @RequestMapping("user")
+@RequestScope
 public class AuthUserController {
 
     private final AuthUserService authUserService;
 
+    private final GlobalProperties globalProperties;
+
+    @Value("${customs.user.name}")
+    private String userName;
+
     @GetMapping("listAll")
     public ResponseData<List<AuthUser>> listAll() {
         return ResponseData.success(authUserService.listAll());
+    }
+
+    @GetMapping("testProperties")
+    public String testProperties() {
+        return MessageFormat.format("author: {0}, userName: {1}", globalProperties.getAuthor(), userName);
     }
 }
