e8ca762fd586ba7948056d281fbba62a8b3a9bdc
dongfang.ding <1041765757@qq.com>
2020-07-13 21:24:20 +0800

    补充openfeign使用姿势

--- service-user-center/src/main/java/com/ddf/cloud/handbook/usercenter/controller/AuthUserController.java
+++ service-user-center/src/main/java/com/ddf/cloud/handbook/usercenter/controller/AuthUserController.java
@@ -1,6 +1,9 @@
 package com.ddf.cloud.handbook.usercenter.controller;
 
+import com.ddf.cloud.handbook.api.model.usercenter.AuthUser;
 import com.ddf.cloud.handbook.core.config.GlobalProperties;
+import com.ddf.cloud.handbook.core.response.ResponseData;
+import com.ddf.cloud.handbook.usercenter.service.AuthUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,9 +11,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.annotation.RequestScope;
 
 import java.text.MessageFormat;
+import java.util.List;
 
 /**
  * <p>description</p >
@@ -22,16 +25,22 @@ import java.text.MessageFormat;
 @RestController
 @RequiredArgsConstructor(onConstructor=@__(@Autowired))
 @RequestMapping("user")
-@RequestScope
 public class AuthUserController {
 
     private final GlobalProperties globalProperties;
 
     private final Environment environment;
 
+    private final AuthUserService authUserService;
+
     @Value("${customs.user.name}")
     private String userName;
 
+
+    /**
+     * 演示主配置文件和共享配置文件的动态刷新
+     * @return
+     */
     @GetMapping("testProperties")
     public String testProperties() {
         return MessageFormat.format("author: {0}, 注入userName: {1}, 从环境变量中取userName: {2}," +
@@ -41,5 +50,10 @@ public class AuthUserController {
                 environment.getProperty("customs.user.name"),
                 environment.getProperty("customs.shard-config.user.name")
         );
+    }
+
+    @GetMapping("listAll")
+    public ResponseData<List<AuthUser>> listAll() {
+        return ResponseData.success(authUserService.listAll());
     }
 }
